f3208559c4711d7652e95447d5e191fc61d74c30
Garret Meier <meier.garret@gmail.com>
2014-03-29 15:09:40 -0500

    Adding some style

--- globals.php
+++ globals.php
@@ -1,11 +1,32 @@
 <?php
 
-$incomes = new array();
-$expenses = new array();
+class Globals{
+	$incomes = new array();
+	$expenses = new array();
 
-define("NONE", 0);
-define("DAILY", 1);
-define("MONTHLY", 2);
-define("YEARLY", 3);
+	define("NONE", 0);
+	define("DAILY", 1);
+	define("MONTHLY", 2);
+	define("YEARLY", 3);
+	
+	function addIncome($income){
+		$incomes[] = $income;
+	}
+	
+	function addExpense($expense){
+		$expenses[] = $expense;
+	}
+	
+	function getIncomes(){
+		return $incomes;
+	}
+	
+	function getExpenses(){
+		return $expenses;
+	}
+	
+	
+
+}
 
 ?>
